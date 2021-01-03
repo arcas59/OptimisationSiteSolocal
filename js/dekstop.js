c5be329cd6f546c2b40226f4c114494b8fdedc70
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2021-01-03 23:52:49 +0100

    Réorganisation du code avec dossier JS et CSS

---
+++ js/dekstop.js
@@ -0,0 +1,19 @@
+window._currentDevice = 'desktop';
+			window.Parameters = window.Parameters || {
+				AjaxContainer: 'div.dmBody',WrappingContainer: 'div.dmOuter',HomeUrl: 'https://www.actuelle-coiffure.com/',AccountUUID: '78f07489ccd443bbab33206136ef075d',SystemID: 'EU_PRODUCTION',SiteAlias: 'e17455a31e5a4221992ed7cebb415a28',SiteId: '409611',SiteType: atob('RFVEQU9ORQ=='),ExternalUid: 'PREMIUM|00395419||||||',IsSiteMultilingual: false,InitialPostAlias: '',InitialDynamicItem: '',InitialPageAlias: 'home',InitialPageUuid: 'f31fae89376f4777b08547c400ebbb87',InitialEncodedPageAlias: 'aG9tZQ==',CurrentPageUrl: '',IsCurrentHomePage: true,AllowAjax: false,AfterAjaxCommand: null,HomeLinkText: 'Back To Home',UseGalleryModule: false,CurrentThemeName: 'Layout Theme',ThemeVersion: '1625',DefaultPageAlias: '',RemoveDID: true,WidgetStyleID : null,IsHeaderFixed: false,IsHeaderSkinny: false,IsBfs: true,LayoutParams : {
+					_navigationAnimationStyle: 'slide',_manifestId : 10600,_device : 'desktop'
+				}
+				,StorePageAlias: 'null',StorePath: '',StoreId: 'null',StoreVersion: 0,StoreBaseUrl: '',StoreCleanUrl: true,StoreDisableScrolling: true,NotificationSubDomain: 'actuelle-coiffure',HasCustomDomain: true,showCookieNotification: true,cookiesNotificationMarkup: ' <div>Nous utilisons des cookies pour optimiser votre exp&eacute;rience sur notre site web. Pour en savoir plus, veuillez acc&eacute;der &agrave; la page Confidentialit&eacute;.<\/div> \n',translatedPageUrl: '',isFastMigrationSite: false,sidebarPosition: 'NA',currentLanguage: 'fr',NavItems: 'W3sidGl0bGUiOiJBY2N1ZWlsIiwiYWxpYXMiOiJob21lIiwicGF0aCI6Ii8iLCJpbk5hdmlnYXRpb24iOnRydWUsInN1Yk5hdiI6W119LHsidGl0bGUiOiJOb3MgcHJlc3RhdGlvbnMgZGUgY29pZmZ1cmUiLCJhbGlhcyI6ImNvdXBlLWNoZXZldXgiLCJwYXRoIjoiL2NvdXBlLWNoZXZldXgiLCJpbk5hdmlnYXRpb24iOnRydWUsInN1Yk5hdiI6W119LHsidGl0bGUiOiJQaG90b3MiLCJhbGlhcyI6InBob3RvcyIsInBhdGgiOiIvcGhvdG9zIiwiaW5OYXZpZ2F0aW9uIjp0cnVlLCJzdWJOYXYiOltdfSx7InRpdGxlIjoiQ29udGFjdCIsImFsaWFzIjoiY29udGFjdCIsInBhdGgiOiIvY29udGFjdCIsImluTmF2aWdhdGlvbiI6dHJ1ZSwic3ViTmF2IjpbXX0seyJ0aXRsZSI6IlBsYW4gZHUgc2l0ZSIsImFsaWFzIjoicGxhbi1kdS1zaXRlIiwicGF0aCI6Ii9wbGFuLWR1LXNpdGUiLCJpbk5hdmlnYXRpb24iOmZhbHNlLCJzdWJOYXYiOltdfSx7InRpdGxlIjoiTWVudGlvbnMgbMOpZ2FsZXMiLCJhbGlhcyI6Im1lbnRpb25zbGVnYWxlcyIsInBhdGgiOiIvbWVudGlvbnNsZWdhbGVzIiwiaW5OYXZpZ2F0aW9uIjpmYWxzZSwic3ViTmF2IjpbXX0seyJ0aXRsZSI6IlZpZSBwcml2w6llIiwiYWxpYXMiOiJ2aWVwcml2ZWUiLCJwYXRoIjoiL3ZpZXByaXZlZSIsImluTmF2aWdhdGlvbiI6ZmFsc2UsInN1Yk5hdiI6W119XQ==',errors: {
+					general: 'There was an error connecting to the page.<br/> Make sure you are not offline.',password: 'Incorrect name/password combination',tryAgain: 'Try again'
+				}
+				,NavigationAreaParams: {
+					ShowBackToHomeOnInnerPages: true,NavbarSize: -1,NavbarLiveHomePage: 'https://www.actuelle-coiffure.com/',BlockContainerSelector: '.dmBody',NavbarSelector: '#dmNav:has(a)',SubNavbarSelector: '#subnav_main'
+				}
+				,hasCustomCode: false,planID: 1457,disableTracking: false
+			};
+			window.Parameters.LayoutID = {
+			};
+			window.Parameters.LayoutID[window._currentDevice] = 6;
+			window.Parameters.LayoutVariationID = {
+			};
+			window.Parameters.LayoutVariationID[window._currentDevice] = 5;
